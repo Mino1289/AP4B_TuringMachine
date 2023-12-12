d0e7b0b1a576496ecfa697d4cfa18f461da04913
Wiqiro <william.imbert@utbm.fr>
2023-12-12 12:54:52 +0100

    feat: added the unsolved criteria cards

--- turingmachine/src/main/java/com/turingmachine/core/CriteriaCardGenerator.java
+++ turingmachine/src/main/java/com/turingmachine/core/CriteriaCardGenerator.java
@@ -186,117 +186,211 @@ public class CriteriaCardGenerator {
                 break;
             case 26:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < 3,
+                        punchCard -> punchCard.yellow < 3,
+                        punchCard -> punchCard.purple < 3,
                 };
                 break;
             case 27:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < 4,
+                        punchCard -> punchCard.yellow < 4,
+                        punchCard -> punchCard.purple < 4,
                 };
                 break;
             case 28:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue == 1,
+                        punchCard -> punchCard.yellow == 1,
+                        punchCard -> punchCard.purple == 1,
                 };
                 break;
             case 29:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue == 3,
+                        punchCard -> punchCard.yellow == 3,
+                        punchCard -> punchCard.purple == 3,
                 };
                 break;
             case 30:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue == 4,
+                        punchCard -> punchCard.yellow == 4,
+                        punchCard -> punchCard.purple == 4,
                 };
                 break;
             case 31:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue > 1,
+                        punchCard -> punchCard.yellow > 1,
+                        punchCard -> punchCard.purple > 1,
                 };
                 break;
             case 32:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue > 3,
+                        punchCard -> punchCard.yellow > 3,
+                        punchCard -> punchCard.purple > 3,
                 };
                 break;
             case 33:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue % 2 == 0,
+                        punchCard -> punchCard.yellow % 2 == 0,
+                        punchCard -> punchCard.purple % 2 == 0,
+                        punchCard -> punchCard.blue % 2 == 1,
+                        punchCard -> punchCard.yellow % 2 == 1,
+                        punchCard -> punchCard.purple % 2 == 1,
                 };
                 break;
             case 34:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue <= punchCard.yellow && punchCard.blue <= punchCard.purple,
+                        punchCard -> punchCard.yellow <= punchCard.blue && punchCard.yellow <= punchCard.purple,
+                        punchCard -> punchCard.purple <= punchCard.blue && punchCard.purple <= punchCard.yellow,
                 };
                 break;
             case 35:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue >= punchCard.yellow && punchCard.blue >= punchCard.purple,
+                        punchCard -> punchCard.yellow >= punchCard.blue && punchCard.yellow >= punchCard.purple,
+                        punchCard -> punchCard.purple >= punchCard.blue && punchCard.purple >= punchCard.yellow,
                 };
                 break;
             case 36:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue + punchCard.yellow + punchCard.purple % 3 == 0,
+                        punchCard -> punchCard.blue + punchCard.yellow + punchCard.purple % 4 == 0,
+                        punchCard -> punchCard.blue + punchCard.yellow + punchCard.purple % 5 == 0,
                 };
                 break;
             case 37:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue + punchCard.yellow == 4,
+                        punchCard -> punchCard.blue + punchCard.purple == 4,
+                        punchCard -> punchCard.yellow + punchCard.purple == 4,
                 };
                 break;
             case 38:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue + punchCard.yellow == 6,
+                        punchCard -> punchCard.blue + punchCard.purple == 6,
+                        punchCard -> punchCard.yellow + punchCard.purple == 6,
                 };
                 break;
             case 39:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue == 1,
+                        punchCard -> punchCard.yellow == 1,
+                        punchCard -> punchCard.purple == 1,
+                        punchCard -> punchCard.blue > 1,
+                        punchCard -> punchCard.yellow > 1,
+                        punchCard -> punchCard.purple > 1,
                 };
                 break;
             case 40:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < 3,
+                        punchCard -> punchCard.yellow < 3,
+                        punchCard -> punchCard.purple < 3,
+                        punchCard -> punchCard.blue == 3,
+                        punchCard -> punchCard.yellow == 3,
+                        punchCard -> punchCard.purple == 3,
+                        punchCard -> punchCard.blue > 3,
+                        punchCard -> punchCard.yellow > 3,
+                        punchCard -> punchCard.purple > 3,
                 };
                 break;
             case 41:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < 4,
+                        punchCard -> punchCard.yellow < 4,
+                        punchCard -> punchCard.purple < 4,
+                        punchCard -> punchCard.blue == 4,
+                        punchCard -> punchCard.yellow == 4,
+                        punchCard -> punchCard.purple == 4,
+                        punchCard -> punchCard.blue > 4,
+                        punchCard -> punchCard.yellow > 4,
+                        punchCard -> punchCard.purple > 4,
                 };
                 break;
             case 42:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < punchCard.yellow && punchCard.blue < punchCard.purple,
+                        punchCard -> punchCard.yellow < punchCard.blue && punchCard.yellow < punchCard.purple,
+                        punchCard -> punchCard.purple < punchCard.blue && punchCard.purple < punchCard.yellow,
+                        punchCard -> punchCard.blue > punchCard.yellow && punchCard.blue > punchCard.purple,
+                        punchCard -> punchCard.yellow > punchCard.blue && punchCard.yellow > punchCard.purple,
+                        punchCard -> punchCard.purple > punchCard.blue && punchCard.purple > punchCard.yellow,
                 };
                 break;
             case 43:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < punchCard.yellow,
+                        punchCard -> punchCard.blue == punchCard.yellow,
+                        punchCard -> punchCard.blue > punchCard.yellow,
+                        punchCard -> punchCard.blue < punchCard.purple,
+                        punchCard -> punchCard.blue == punchCard.purple,
+                        punchCard -> punchCard.blue > punchCard.purple,
                 };
                 break;
             case 44:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.yellow < punchCard.blue,
+                        punchCard -> punchCard.yellow == punchCard.blue,
+                        punchCard -> punchCard.yellow > punchCard.blue,
+                        punchCard -> punchCard.yellow < punchCard.purple,
+                        punchCard -> punchCard.yellow == punchCard.purple,
+                        punchCard -> punchCard.yellow > punchCard.purple,
                 };
                 break;
             case 45:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.nCount(1) == 0,
+                        punchCard -> punchCard.nCount(1) == 1,
+                        punchCard -> punchCard.nCount(1) == 2,
+                        punchCard -> punchCard.nCount(1) == 3,
+                        punchCard -> punchCard.nCount(3) == 0,
+                        punchCard -> punchCard.nCount(3) == 1,
+                        punchCard -> punchCard.nCount(3) == 2,
+                        punchCard -> punchCard.nCount(3) == 3,
                 };
                 break;
             case 46:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.nCount(3) == 0,
+                        punchCard -> punchCard.nCount(3) == 1,
+                        punchCard -> punchCard.nCount(3) == 2,
+                        punchCard -> punchCard.nCount(3) == 3,
+                        punchCard -> punchCard.nCount(4) == 0,
+                        punchCard -> punchCard.nCount(4) == 1,
+                        punchCard -> punchCard.nCount(4) == 2,
+                        punchCard -> punchCard.nCount(4) == 3,
                 };
                 break;
             case 47:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.nCount(1) == 0,
+                        punchCard -> punchCard.nCount(1) == 1,
+                        punchCard -> punchCard.nCount(1) == 2,
+                        punchCard -> punchCard.nCount(1) == 3,
+                        punchCard -> punchCard.nCount(4) == 0,
+                        punchCard -> punchCard.nCount(4) == 1,
+                        punchCard -> punchCard.nCount(4) == 2,
+                        punchCard -> punchCard.nCount(4) == 3,
                 };
                 break;
             case 48:
                 criterias = new Criteria[] {
-
+                        punchCard -> punchCard.blue < punchCard.yellow,
+                        punchCard -> punchCard.blue < punchCard.purple,
+                        punchCard -> punchCard.yellow < punchCard.purple,
+                        punchCard -> punchCard.blue == punchCard.yellow,
+                        punchCard -> punchCard.blue == punchCard.purple,
+                        punchCard -> punchCard.yellow == punchCard.purple,
+                        punchCard -> punchCard.blue > punchCard.yellow,
+                        punchCard -> punchCard.blue > punchCard.purple,
+                        punchCard -> punchCard.yellow > punchCard.purple,
                 };
                 break;
             default:
