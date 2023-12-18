e5b2f9635a89671c5ba9a9cf7586edc254cbb8a2
Mino1289 <kretzvictor@orange.fr>
2023-12-18 10:48:34 +0100

    feat: change Title !

--- turingmachine/src/main/java/com/turingmachine/gui/App.java
+++ turingmachine/src/main/java/com/turingmachine/gui/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("main-menu"), 640, 480);
         stage.setScene(scene);
+        stage.setTitle("TuringMachine");
+        // stage.getIcons().add(Image(""))
         stage.show();
     }
 
