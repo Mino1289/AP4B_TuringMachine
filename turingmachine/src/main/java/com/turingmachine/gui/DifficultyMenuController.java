1db1c0698b01447b8d9493ca0f0af103d546d9d5
Wiqiro <william.imbert@utbm.fr>
2023-12-18 10:26:03 +0100

    feat: difficulty mode selection

---
+++ turingmachine/src/main/java/com/turingmachine/gui/DifficultyMenuController.java
@@ -0,0 +1,20 @@
+package com.turingmachine.gui;
+
+import javafx.fxml.FXML;
+
+public class DifficultyMenuController {
+    @FXML
+    private void selectEasyMode() {
+        System.out.println("Easy mode selected!");
+    }
+
+    @FXML
+    private void selectMediumMode() {
+        System.out.println("Medium mode selected!");
+    }
+
+    @FXML
+    private void selectHardMode() {
+        System.out.println("Hard mode selected!");
+    }
+}
