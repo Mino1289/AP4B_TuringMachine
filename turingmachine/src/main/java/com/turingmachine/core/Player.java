de253dc7ac645a2093f4ca104f72db71db5558b6
Mino1289 <kretzvictor@orange.fr>
2024-01-05 21:33:37 +0100

    ref: removing unused stuff

--- turingmachine/src/main/java/com/turingmachine/core/Player.java
+++ turingmachine/src/main/java/com/turingmachine/core/Player.java
@@ -1,22 +1,12 @@
 package com.turingmachine.core;
 
 public class Player {
-    private int testCount;
     private int currentTestCount = 3;
     private String username;
     private PunchCard punchCard;
 
     public Player(String _username) {
-        this.testCount = 0;
         this.username = _username;
-    }
-
-    public void incrementTestCount() {
-        this.testCount++;
-    }
-
-    public int getTestCount() {
-        return this.testCount;
     }
 
     public String getUsername() {
